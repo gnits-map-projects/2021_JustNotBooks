86953d790b197bc1613cdf7955701c157e858ab9
R Saai Arputa <rsaaiarputa16@gmail.com>
2021-04-25 09:05:12 +0530

    ui changes

--- ui/src/Nav.js
+++ ui/src/Nav.js
@@ -41,11 +41,7 @@ class Nav extends React.Component {
               <li><a href="/seller">Transaction</a></li>
               <li>
                 <input type="search" placeholder="Search for an item" width="500px" value={this.state.search} onChange={this.handleChange} name="search" />
-<<<<<<< HEAD
-                <button className="btnstyle2" onClick={this.handleSubmit}><img src={search}/></button>
-=======
                 <button className="btnstyle2" onClick={this.handleSubmit}><img src={search} alt="Logo" width="25px" height="25px" /></button>
->>>>>>> ff352ee62521d4ed7ce6c00d2147df2fb8d6185a
               </li>
               <li>< a class="p" href="/survey"><img src={ic} /></a></li>
               <li><a class="p" href="/profile">{sessionStorage.getItem("name")}'s Profile</a></li>
